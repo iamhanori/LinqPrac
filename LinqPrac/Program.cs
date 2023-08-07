5bdf20e12ed4964d974bcfc799f1786a29f21001
Hanul Kim <s2122@e-mirim.hs.kr>
2023-08-07 12:12:32 +0900

    Linq의 결과를 리스트 또는 배열로 변환하기

--- LinqPrac/Program.cs
+++ LinqPrac/Program.cs
@@ -31,6 +31,8 @@ namespace LinqPrac
             {
                 Console.WriteLine(item);
             }
+            output = output2.ToList<int>();
+            int[] output3 = output2.ToArray<int>();
         }
     }
 }
